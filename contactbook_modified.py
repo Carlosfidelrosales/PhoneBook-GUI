87435c92fa908199f105ececb5ccbc6e30530d1d
Carlos Fidel Rosales <fidelcarlos302@gmail.com>
2023-02-13 20:58:38 +0800

    Added the size of the interface and make it non-resizable.

--- contactbook_modified.py
+++ contactbook_modified.py
@@ -2,3 +2,8 @@ from tkinter import *
 import csv
 from tkinter import messagebox
 import tkinter.font
+
+dataContact = Tk()
+dataContact.title("Contact Book")
+dataContact.geometry("1300x375")
+dataContact.resizable(False, False)
