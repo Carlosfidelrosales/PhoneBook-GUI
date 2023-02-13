2b882d89159b21a7a1b0578f0cd2738efbf9629b
Carlos Fidel Rosales <fidelcarlos302@gmail.com>
2023-02-13 21:08:14 +0800

    Added a def functions that will read and write on the CSV file.

--- contactbook_modified.py
+++ contactbook_modified.py
@@ -14,3 +14,19 @@ background_label = Label(dataContact, image=imageFile, anchor="center")
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 contactList = []
+
+def InterpetCSVFile():
+    global header
+    with open('StudentData.csv') as csvfile:
+        csv_reader = csv.reader(csvfile,delimiter=',')
+        header = next(csv_reader)
+        for row in csv_reader:
+            contactList.append(row)	
+    print(contactList)
+
+def EditCSVFile(contactList):
+    with open('StudentData.csv','w', newline='') as csv_file:
+        writecsv = csv.writer(csv_file,delimiter=',')
+        writecsv.writerow(header)
+        for row in contactList:
+            writecsv.writerow(row)
