699c9275b192764bfe79753397213afa5b68a515
Carlos Fidel Rosales <fidelcarlos302@gmail.com>
2023-02-13 21:02:40 +0800

    Added a configuration for the background of the GUI. Also, I added a empty list/array of contact list.

--- contactbook_modified.py
+++ contactbook_modified.py
@@ -7,3 +7,10 @@ dataContact = Tk()
 dataContact.title("Contact Book")
 dataContact.geometry("1300x375")
 dataContact.resizable(False, False)
+
+FRAME = Frame(dataContact, bg="blue", height=100, width=300)
+imageFile = PhotoImage(file = "phonebook.png")
+background_label = Label(dataContact, image=imageFile, anchor="center")
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+contactList = []
