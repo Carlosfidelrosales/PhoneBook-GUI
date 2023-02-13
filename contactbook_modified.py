5afcc0a71834996972d3620a8801a944d541ada9
Carlos Fidel Rosales <fidelcarlos302@gmail.com>
2023-02-13 22:11:10 +0800

    Corrected the filepath of the image background and also the filepath of the CSV

--- contactbook_modified.py
+++ contactbook_modified.py
@@ -9,7 +9,7 @@ dataContact.geometry("1300x375")
 dataContact.resizable(False, False)
 
 FRAME = Frame(dataContact, bg="blue", height=100, width=300)
-imageFile = PhotoImage(file = "phonebook.png")
+imageFile = PhotoImage(file = "PhoneBook-GUI/phonebook.png")
 background_label = Label(dataContact, image=imageFile, anchor="center")
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
@@ -17,7 +17,7 @@ contactList = []
 
 def InterpetCSVFile():
     global header
-    with open('StudentData.csv') as csvfile:
+    with open('Phonebook-GUI/StudentData.csv') as csvfile:
         csv_reader = csv.reader(csvfile,delimiter=',')
         header = next(csv_reader)
         for row in csv_reader:
